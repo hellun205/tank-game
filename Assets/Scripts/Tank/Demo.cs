fc6b3b81760334af37291033d40cbafc6240f201
hellun205 <hellun515@gmail.com>
2023-04-25 04:10:38 +0900

    main menu scene

---
+++ Assets/Scripts/Tank/Demo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Tank {
+  public class Demo : MonoBehaviour {
+    public Animator[] animators;
+
+    private void Start() {
+      foreach (var anim in animators) {
+        anim.SetInteger("Direction", 1);
+        anim.SetFloat("Speed", 5f);
+      }
+    }
+  }
+}
